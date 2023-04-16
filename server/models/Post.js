a3e235632458b90e83c85fca14c3b84b2eec67a0
milo cohen-elyanow <milocohenelyanow@gmail.com>
2023-04-16 12:13:59 -0500

    just some inital work and models

---
+++ server/models/Post.js
@@ -0,0 +1,13 @@
+// post should be able to have pictures videos and the like, we will store it in our database to always be accesable 
+//post will have a user section, a description section, and a comments section, aswell as likes,
+
+//post pictures will be downloaded to server, stored, and path will be accessable to post schema,
+
+
+const {schema, model} = require("mongoose");
+
+const postSchema = new Schema({
+  post: {
+    type: 
+  }
+})
